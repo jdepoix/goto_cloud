80758e349021328f21e2943149cbc437ab4fb0b8
Jonas Depoix <jdepoix@seibert-media.net>
2017-03-31 14:44:41 +0200

    operation system support refactored

--- goto_cloud/operating_system_support/operating_system_support.py
+++ goto_cloud/operating_system_support/operating_system_support.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta, abstractclassmethod
+from abc import ABCMeta, abstractmethod
 
 from dict_utils.public import DictUtils
 
@@ -17,49 +17,111 @@ class OperatingSystemRelations():
     system.
     """
 
-    @classmethod
-    def get_subsystems(cls, operating_system):
+    def __init__(self, operating_system):
+        """
+        
+        :param operating_system: the operating system, on whom's relations you want to work
+        :type operating_system: str  
+        """
+        self.operating_system = operating_system
+
+    def is_parent_of(self, related_operating_system):
+        """
+        checks if this operating system is parent of the given operating system
+        
+        :param related_operating_system: the related operating system
+        :type related_operating_system: str
+        :return: whether it is a parent or not
+        :rtype: bool
+        """
+        return related_operating_system in self.get_subsystems()
+
+    def is_child_of(self, related_operating_system):
+        """
+        checks if this operating system is child of the given operating system
+
+        :param related_operating_system: the related operating system
+        :type related_operating_system: str
+        :return: whether it is a child or not
+        :rtype: bool
+        """
+        return self.operating_system in OperatingSystemRelations(related_operating_system).get_subsystems()
+
+    def get_subsystems(self):
         """
         returns the operating systems, which the given operating system is related parent to
         
-        :param operating_system: the operating system which is the parent
-        :type operating_system: str
         :return: list of subsystems
         :rtype: list
         """
-        subsystems = DictUtils.find_sub_dict_by_key(cls._RELATIONS, operating_system)
-        return DictUtils.flatten_child_elements(subsystems) if subsystems else None
+        subsystems = DictUtils.find_sub_dict_by_key(self._RELATIONS, self.operating_system)
+        return DictUtils.flatten_child_elements(subsystems) if subsystems else []
 
 
-class PartiallySupported(metaclass=ABCMeta):
+class AbstractedRemoteHostOperator(metaclass=ABCMeta):
     """
-    represents a class which is only supported by limited operating systems
+    a operation which executes on a given RemoteHost and abstracts the operating system support away
     """
-    @classmethod
-    def is_supported(cls, operating_system):
-        """
-        checks if the given operating system is supported
-        
-        :param operating_system: the operating system to check support for
-        :type operating_system: str
-        :return: if it is supported or not
-        :rtype: bool
-        """
-        supported_systems = cls._get_supported_operating_systems()
+    def __init__(self, remote_host):
+        self.operator = self._get_supported_operator(remote_host)
 
-        return (
-            operating_system in supported_systems
-            or any(
-                operating_system in OperatingSystemRelations.get_subsystems(supported_system)
-                for supported_system in supported_systems
-            )
-        )
-
-    @abstractclassmethod
-    def _get_supported_operating_systems(cls):
+    def _get_supported_operator(self, remote_host):
         """
+        returns the supported operator class for a given RemoteHost  
         
-        :return: a tuple of supported operating systems
-        :rtype: (str,)
+        :param remote_host: the remote host you want to return the supported operator for
+        :type remote_host: remote_host.public.RemoteHost
+        :return: a operator class, which is supported by the given RemoteHost
+        :rtype: Any.__class__
+        :raises OperatingSystem.NotSupportedException: raised in case, the operating system of the given RemoteHost is
+        not supported at all
+        """
+        supported_operator = self._get_directly_supported_operator(remote_host)
+
+        if supported_operator:
+            return self._init_operator_class(supported_operator)
+
+        related_supported_operator = self._get_related_supported_operator(remote_host)
+
+        if related_supported_operator:
+            return self._init_operator_class(related_supported_operator)
+
+        raise OperatingSystem.NotSupportedException()
+
+
+    def _get_directly_supported_operator(self, remote_host):
+        for operating_systems, operator_class in self._get_operating_systems_to_supported_operation_mapping().items():
+            if remote_host.os in operating_systems:
+                return operator_class
+        return None
+
+    def _get_related_supported_operator(self, remote_host):
+        for operating_systems, operator_class in self._get_operating_systems_to_supported_operation_mapping().items():
+            remote_host_operating_system_relations = OperatingSystemRelations(remote_host.os)
+            if any(
+                remote_host_operating_system_relations.is_parent_of(operating_system)
+                for operating_system in operating_systems
+            ):
+                return operator_class
+        return None
+
+    @abstractmethod
+    def _get_operating_systems_to_supported_operation_mapping(self):
+        """
+
+        :return: maps a tuple of operating systems, onto the operation classes, which support it
+        :rtype: {(str,): Any}
+        """
+        pass
+
+    @abstractmethod
+    def _init_operator_class(self, operator_class):
+        """
+        initializes the supported operator class
+        
+        :param operator_class: the operator class
+        :type operator_class: Any.__class__
+        :return: an instance of the operator class
+        :rtype: Any
         """
         pass
--- goto_cloud/operating_system_support/tests/test_operating_system_support.py
+++ goto_cloud/operating_system_support/tests/test_operating_system_support.py
@@ -1,6 +1,12 @@
-from unittest import TestCase
+import unittest
 
-from ..operating_system_support import OperatingSystemRelations, PartiallySupported
+from django.test import TestCase
+
+from operating_system.public import OperatingSystem
+
+from remote_host.public import RemoteHost
+
+from ..operating_system_support import OperatingSystemRelations, AbstractedRemoteHostOperator
 
 
 RELATION_MOCK = {
@@ -13,42 +19,88 @@ RELATION_MOCK = {
         },
         '8': '9',
         '10': '11',
+    },
+    '12': {
+        '13': '14',
+        '15': '16',
     }
 }
 
 
-class PartiallySupportedTestClass(PartiallySupported):
-    @classmethod
-    def _get_supported_operating_systems(cls):
-        return '2', '8'
+class TestOperationA():
+    pass
 
 
-class TestOperatingSystemRelations(TestCase):
+class TestOperationB():
+    pass
+
+
+class AbstractedRemoteHostOperatorTestImplementation(AbstractedRemoteHostOperator):
+    def _get_operating_systems_to_supported_operation_mapping(self):
+        return {
+            ('2', '8',): TestOperationA,
+            ('13',): TestOperationB,
+        }
+
+    def _init_operator_class(self, operator_class):
+        return operator_class()
+
+
+class TestOperatingSystemRelations(unittest.TestCase):
     def setUp(self):
         OperatingSystemRelations._RELATIONS = RELATION_MOCK
 
     def test_get_subsystems(self):
         self.assertEqual(
-            set(OperatingSystemRelations.get_subsystems('2')),
+            set(OperatingSystemRelations('2').get_subsystems()),
             {'3', '4', '5', '6', '7'}
         )
 
     def test_get_subsystems__no_subsystems(self):
-        self.assertIsNone(OperatingSystemRelations.get_subsystems('11'))
+        self.assertEquals(OperatingSystemRelations('11').get_subsystems(), [])
 
     def test_get_subsystems__no_relations_known(self):
-        self.assertIsNone(OperatingSystemRelations.get_subsystems('999'))
+        self.assertEquals(OperatingSystemRelations('999').get_subsystems(), [])
+
+    def test_is_parent_of(self):
+        self.assertTrue(OperatingSystemRelations('3').is_parent_of('5'))
+
+    def test_is_child_of(self):
+        self.assertTrue(OperatingSystemRelations('3').is_child_of('1'))
 
 
-class TestPartiallySupported(TestCase):
+class TestAbstractedRemoteHostOperator(TestCase):
     def setUp(self):
         OperatingSystemRelations._RELATIONS = RELATION_MOCK
 
-    def test_is_supported(self):
-        self.assertTrue(PartiallySupportedTestClass.is_supported('2'))
+    def test_initialization(self):
+        self.assertTrue(
+            isinstance(
+                AbstractedRemoteHostOperatorTestImplementation(RemoteHost.objects.create(os='8')).operator,
+                TestOperationA
+            )
+        )
+        self.assertTrue(
+            isinstance(
+                AbstractedRemoteHostOperatorTestImplementation(RemoteHost.objects.create(os='13')).operator,
+                TestOperationB
+            )
+        )
 
-    def test_is_supported__related(self):
-        self.assertTrue(PartiallySupportedTestClass.is_supported('4'))
+    def test_initialization__related(self):
+        self.assertTrue(
+            isinstance(
+                AbstractedRemoteHostOperatorTestImplementation(RemoteHost.objects.create(os='1')).operator,
+                TestOperationA
+            )
+        )
+        self.assertTrue(
+            isinstance(
+                AbstractedRemoteHostOperatorTestImplementation(RemoteHost.objects.create(os='12')).operator,
+                TestOperationB
+            )
+        )
 
-    def test_is_supported__not_supported(self):
-        self.assertFalse(PartiallySupportedTestClass.is_supported('10'))
+    def test_initialization__not_supported(self):
+        with self.assertRaises(OperatingSystem.NotSupportedException):
+            AbstractedRemoteHostOperatorTestImplementation(RemoteHost.objects.create(os='16'))
