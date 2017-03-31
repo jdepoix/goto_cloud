60d909dc07ce934e52659d2090c42ba98790d726
Jonas Depoix <jdepoix@seibert-media.net>
2017-03-31 11:37:04 +0200

    MigrationPlan models added

---
+++ goto_cloud/migration_plan/models.py
@@ -0,0 +1,10 @@
+from django.contrib.postgres.fields import JSONField
+
+from tracked_model.public import TrackedModel
+
+
+class MigrationPlan(TrackedModel):
+    """
+    represents the plan which is used for a migration
+    """
+    plan = JSONField()
