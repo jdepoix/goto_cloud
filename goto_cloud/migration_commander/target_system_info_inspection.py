c740cdf835a763db98bd97fc8f3475655d7fa445
Jonas Depoix <jdepoix@seibert-media.net>
2017-04-21 17:49:03 +0200

    GetTargetSystemInfoCommand implemented

---
+++ goto_cloud/migration_commander/target_system_info_inspection.py
@@ -0,0 +1,12 @@
+from command.command import SourceCommand
+from system_info_inspection.system_info_inspection import RemoteHostSystemInfoGetter
+
+
+class GetTargetSystemInfoCommand(SourceCommand):
+    """
+    Takes care of providing the target with system information after it has been created
+    """
+    def execute(self):
+        target_remote_host = self._source.target.remote_host
+        target_remote_host.system_info = RemoteHostSystemInfoGetter(target_remote_host).get_system_info()
+        target_remote_host.save()
