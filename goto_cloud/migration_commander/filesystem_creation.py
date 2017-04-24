b6908157a798c22dfe11f3596bcb06715cc3b137
Jonas Depoix <jdepoix@seibert-media.net>
2017-04-24 13:41:50 +0200

    filesystem creation implemented

---
+++ goto_cloud/migration_commander/filesystem_creation.py
@@ -0,0 +1,90 @@
+from remote_host_command.public import RemoteHostCommand
+
+from .device_modification import DeviceModifyingCommand
+
+
+class CreateFilesystemsCommand(DeviceModifyingCommand):
+    """
+    takes care, of creating the filesystems
+    """
+    class UnsupportedFilesystemException(Exception):
+        """
+        raised if a filesystem is not supported
+        """
+        pass
+
+    def execute(self):
+        errors = self._execute_on_every_device(self._create_filesystem_on_disk, self._create_filesystem_on_partition)
+
+        if errors:
+            raise CreateFilesystemsCommand.UnsupportedFilesystemException(
+                'While trying to create the partitions, the following errors occurred. Please resolves these manually '
+                'and then skip this step:\n{errors}'.format(errors='\n\n'.join(errors))
+            )
+
+    def _create_filesystem_on_partition(self, remote_executor, source_device, target_device, partition_device):
+        """
+        creates a filesystem on a partition
+        
+        :param remote_executor: remote executor to use for execution
+        :type remote_executor: RemoteHostExecutor 
+        :param source_device: the source device
+        :type source_device: (str, dict)
+        :param target_device: the target device
+        :type target_device_id: (str, dict)
+        :param partition_device: the original partition device
+        :type partition_device: (str, dict)
+        :return: a error which occurred during execution (in case it did...)
+        :rtype: str
+        """
+        return self._create_filesystem(remote_executor, partition_device, target_device[0] + partition_device[0][-1])
+
+    def _create_filesystem_on_disk(self, remote_executor, source_device, target_device):
+        """
+        creates a filesystem on a disk
+
+        :param remote_executor: remote executor to use for execution
+        :type remote_executor: RemoteHostExecutor 
+        :param source_device: the source device
+        :type source_device: (str, dict)
+        :param target_device: the target device
+        :type target_device: (str, dict)
+        :return: a error which occurred during execution (in case it did...)
+        :rtype: str
+        """
+        return self._create_filesystem(remote_executor, source_device, target_device[0])
+
+    def _create_filesystem(self, remote_executor, source_device, target_device_id):
+        """
+        creates a filesystem independent of the device type
+
+        :param remote_executor: remote executor to use for execution
+        :type remote_executor: RemoteHostExecutor 
+        :param source_device: the source device
+        :type source_device: (str, dict)
+        :param target_device: the target device id
+        :type target_device: str
+        :return: a error which occurred during execution (in case it did...)
+        :rtype: str
+        """
+        if source_device[1]['fs']:
+            if source_device[1]['fs'] in self._target.blueprint['commands']['create_filesystem']:
+                remote_executor.execute(
+                    RemoteHostCommand(
+                        self._target.blueprint['commands']['create_filesystem'][source_device[1]['fs']]
+                    ).render(
+                        device='/dev/{device_id}'.format(device_id=target_device_id),
+                        **{'uuid': source_device[1]['uuid']} if source_device[1]['uuid'] else {},
+                        **{'label': source_device[1]['label']} if source_device[1]['label'] else {},
+                    )
+                )
+            else:
+                return (
+                    'The device {source_device_id} can not be replicated, since the filesystem {filesystem} is not '
+                    'supported!'.format(
+                        source_device_id=source_device[0],
+                        filesystem=source_device[1]['fs']
+                    )
+                )
+
+        return None
---
+++ goto_cloud/migration_commander/tests/test_filesystem_creation.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+
+from source.public import Source
+
+from remote_host.public import RemoteHost
+
+from migration_plan_parsing.public import MigrationPlanParser
+
+from test_assets.public import TestAsset
+
+from ..filesystem_creation import CreateFilesystemsCommand
+from ..target_system_info_inspection import GetTargetSystemInfoCommand
+
+from .utils import PatchTrackedRemoteExecution
+
+
+class TestCreateFilesystemsCommand(TestCase, metaclass=PatchTrackedRemoteExecution):
+    def _init_test_data(self, source_host, target_host):
+        MigrationPlanParser().parse(TestAsset.MIGRATION_PLAN_MOCK)
+
+        self.source = Source.objects.get(remote_host__address=source_host)
+        self.source.target.remote_host = RemoteHost.objects.create(address=target_host)
+        self.source.target.save()
+
+        GetTargetSystemInfoCommand(self.source).execute()
+
+        self.source.target.device_mapping = {
+            'vdb': 'vda',
+            'vdc': 'vdb',
+            'vdd': 'vdc',
+        }
+        self.source.target.save()
+
+    def test_execute(self):
+        self._init_test_data('ubuntu16', 'target__filesystem_creation')
+
+        CreateFilesystemsCommand(self.source).execute()
+
+        self.assertIn('sudo mkfs.ext4 -u 549c8755-2757-446e-8c78-f76b50491f21 /dev/vdb1', self.executed_commands)
+        self.assertIn('sudo mkfs.ext3 -u d04ba532-cd2d-4406-a5ef-114acf019cc8 /dev/vdc', self.executed_commands)
+        self.assertIn('sudo mkfs.ext4 -u 53ad2170-488d-481a-a6ab-5ce0e538f247 /dev/vdd1', self.executed_commands)
+        self.assertIn('sudo mkfs.ext4 -u bcab224c-8407-4783-8cea-f9ea4be3fabf /dev/vdd2', self.executed_commands)
+
+    def test_execute__non_supported_filesystem(self):
+        self._init_test_data('ubuntu16__lvm', 'target__filesystem_creation')
+
+        with self.assertRaises(CreateFilesystemsCommand.UnsupportedFilesystemException):
+            CreateFilesystemsCommand(self.source).execute()
