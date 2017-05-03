971021b116556f281a19d3d03335f0d1a2bfaf50
Jonas Depoix <jdepoix@seibert-media.net>
2017-05-03 13:48:03 +0200

    tests added for source file location resolving

--- goto_cloud/migration_commander/tests/test_source_file_location_resolving.py
+++ goto_cloud/migration_commander/tests/test_source_file_location_resolving.py
@@ -1,12 +1,31 @@
-from django.test import TestCase
+from ..source_file_location_resolving import SourceFileLocationResolver
+
+from .utils import MigrationCommanderTestCase
 
 
-class TestSourceFileLocationResolver(TestCase):
+class TestSourceFileLocationResolver(MigrationCommanderTestCase):
+    def _init_test_data(self, source_host, target_host):
+        super()._init_test_data(source_host, target_host)
+        self.resolver = SourceFileLocationResolver(self.source)
+
     def test_resolve__root_path(self):
-        pass
+        self._init_test_data('ubuntu16', 'target__device_identification')
+
+        self.assertEqual(
+            self.resolver.resolve('/etc/fstab'),
+            self.source.target.device_mapping['vda']['children']['vda1']['mountpoint'] + '/etc/fstab'
+        )
 
     def test_resolve__nested_path(self):
-        pass
+        self._init_test_data('ubuntu16', 'target__device_identification')
+
+        self.assertEqual(
+            self.resolver.resolve('/mnt/vdc2/test/path/file'),
+            self.source.target.device_mapping['vdc']['children']['vdc2']['mountpoint'] + '/test/path/file'
+        )
 
     def test_resolve__fail_on_relative_path(self):
-        pass
+        self._init_test_data('ubuntu16', 'target__device_identification')
+
+        with self.assertRaises(SourceFileLocationResolver.InvalidPathException):
+            self.resolver.resolve('relative/path')
