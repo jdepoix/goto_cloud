c66d5d934e487adbe2e814a2237117fac6bac543
Jonas Depoix <jdepoix@seibert-media.net>
2017-05-09 10:26:07 +0200

    test added to bootloader reinstallation module

--- goto_cloud/migration_commander/tests/test_bootloader_reinstallation.py
+++ goto_cloud/migration_commander/tests/test_bootloader_reinstallation.py
@@ -88,3 +88,20 @@ class TestBootloaderReinstallation(MigrationCommanderTestCase):
             ),
             self.executed_commands
         )
+
+    def test_execute__chrooted_into_mounted_root_and_install_bootloader_with_root_mountpoint_on_disk(self):
+        self._init_test_data('ubuntu16', 'target__device_identification')
+
+        self.source.target.device_mapping['vda']['mountpoint'] = \
+            self.source.target.device_mapping['vda']['children']['vda1']['mountpoint']
+        self.source.target.save()
+
+        BootloaderReinstallation(self.source).execute()
+
+        self.assertIn(
+            'sudo chroot {mounted_root} sudo grub-install --boot-directory=/boot /dev/{mounted_root_device_id}'.format(
+                mounted_root=self.source.target.device_mapping['vda']['mountpoint'],
+                mounted_root_device_id=self.source.target.device_mapping['vda']['id'],
+            ),
+            self.executed_commands
+        )
