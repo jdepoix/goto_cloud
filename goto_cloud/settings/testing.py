b364984a96109fe7d28a61f4530132cbc0be1abf
Jonas Depoix <jdepoix@seibert-media.net>
2017-05-18 17:09:57 +0200

    .travis.yml adjusted

--- goto_cloud/settings/testing.py
+++ goto_cloud/settings/testing.py
@@ -1,14 +1,3 @@
 from .base import *
 
 ENVIRONMENT = 'testing'
-
-DATABASES = {
-    'default': {
-        "USER": "postgres",
-        "HOST": "localhost",
-        "PASSWORD": "",
-        "PORT": 5432,
-        "NAME": "goto_cloud",
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    }
-}
